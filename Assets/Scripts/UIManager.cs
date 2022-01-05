ce9af054598cdf28d75d3034682bc7bf44a2c628
MaxRMod <maximilian@modlinger.de>
2022-01-05 20:53:09 +0100

    MainMenu
    Start hinzugefügt, muss noch personalisiert werden.
    Bare Minimum erstmal  implementiert

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+        public void LoadLevelByIndex(int levelIndex){
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
+    public void loadLevelByName(string levelName){
+
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void resetStats(){
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+        resetStats();
+        SceneManager.LoadScene("Spiellevel");
+
+        }
+
+}
+}
