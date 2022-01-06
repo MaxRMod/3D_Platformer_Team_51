8682cd9b138faa62e4026fe9dc8fb36ad3778c9f
MaxRMod <maximilian@modlinger.de>
2022-01-06 18:00:36 +0100

    PauseMenu
    UIManager For Game hier steht alles drin wie der Pausierer funktioniert, Options funktioniert noch nicht

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -22,7 +22,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale=1;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/UIManagerForGame.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIManagerForGame : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject pausePanel;           
+    private bool isPaused=false;       
+    public void LoadLevelByIndex(int levelIndex){
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
+    public void LoadLevelByName(string levelName){
+
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void ResetStats(){
+
+
+    }
+
+    public void ResumeFromPause(){
+        pausePanel.SetActive(false);
+        Time.timeScale=1;
+        isPaused=false;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape)&&!isPaused)
+        {
+                Time.timeScale=0;
+                pausePanel.SetActive(true);
+                isPaused=true;
+
+        }
+
+
+    }
+
+}
