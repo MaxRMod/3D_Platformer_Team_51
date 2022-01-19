71ab384eb408249c0ab9b21f811a724017ee6d19
J3rry <jerrygamerhd@gmail.com>
2022-01-19 23:03:10 +0100

    Added spin to power up items, added sound for key item

---
+++ Assets/Scripts/SimpleCollectibleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCollectibleScript : MonoBehaviour {
+	[SerializeField]
+	public float rotationSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+	}
+
+
+}
