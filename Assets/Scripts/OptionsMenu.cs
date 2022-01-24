414f8f71287e9cb50ad63ce67dbc6917cdcbb9ae
SillesianGuy <jakub.jan.luczak@gmail.com>
2022-01-24 10:48:00 +0100

    Localization en/de/pl
    English/German/Polish localization added

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Localization.Settings;
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -13,9 +14,17 @@ public class OptionsMenu : MonoBehaviour
     }
 
     //Quality
-    public void SetQuality(int qualityIndex)
+
+public IEnumerator SetLanguage()
     {
-        QualitySettings.SetQualityLevel(qualityIndex);
+        // Wait for the localization system to initialize, loading Locales, preloading, etc.
+        yield return LocalizationSettings.InitializationOperation;
+
+        // This variable selects the language. For example, if in the table your first language is English then 0 = English. If the second language in the table is Russian then 1 = Russian etc.
+        int i = 0;
+
+// This part changes the language
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[i];
     }
 
     public AudioMixer volumeMixer;
