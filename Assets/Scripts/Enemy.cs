a0e9ed4471275fdb4bd57dd1e81a2b5620ed91c9
MaxRMod <maximilian@modlinger.de>
2022-01-25 21:16:02 +0100

    Bombe fertig implementiert
    Mal gucken, falls noch etwas fehlt mache ich das noch

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
     public float speed=2;
     public bool invincible=false;
     public float bumpSpeed=10;
-    private Rigidbody enemyrigidbody;
+    private Rigidbody enemyRigidbody;
 
     public void Awake(){
 
-        enemyrigidbody=GetComponent<Rigidbody>();
+        enemyRigidbody=GetComponent<Rigidbody>();
 
     }
 
     public void FixedUpdate(){
-        enemyrigidbody.velocity=new Vector3(speed,enemyrigidbody.velocity.y,0);
+        enemyRigidbody.velocity=new Vector3(speed,enemyRigidbody.velocity.y,0);
 
     }
     public void OnTriggerEnter(Collider other){
