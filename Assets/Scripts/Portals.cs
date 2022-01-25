56a9409f4840bb9ab09aff0e4fc7ab685d10d23b
J3rry <jerrygamerhd@gmail.com>
2022-01-25 13:06:33 +0100

    Added working portals
    stills need some tweaks, and particle systems

---
+++ Assets/Scripts/Portals.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portals : MonoBehaviour
+{
+    [SerializeField]private GameObject portal2;
+    //[SerializeField]private Transform teleportPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.CompareTag("Player"))
+        {
+            if (gameObject.CompareTag("Portal2")) {
+                other.transform.position = new Vector3(portal2.transform.position.x + 0.75f, portal2.transform.position.y + 1, portal2.transform.position.z + 1);
+            } else if (gameObject.CompareTag("Portal1")) {
+                other.transform.position = new Vector3(portal2.transform.position.x - 0.75f, portal2.transform.position.y + 1, portal2.transform.position.z - 1);
+            }
+            other.transform.rotation = portal2.transform.rotation;
+        }
+    }
+}
