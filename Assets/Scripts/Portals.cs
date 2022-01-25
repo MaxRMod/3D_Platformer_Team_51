a3db69f00bf9434d77f81f5eed2dbe215d69906f
J3rry <jerrygamerhd@gmail.com>
2022-01-25 15:17:57 +0100

    Added portal particle system

--- Assets/Scripts/Portals.cs
+++ Assets/Scripts/Portals.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Portals : MonoBehaviour
 {
     [SerializeField]private GameObject portal2;
+    [SerializeField]private GameObject offset;
     //[SerializeField]private Transform teleportPoint;
 
 
@@ -25,9 +26,9 @@ public class Portals : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             if (gameObject.CompareTag("Portal2")) {
-                other.transform.position = new Vector3(portal2.transform.position.x + 0.75f, portal2.transform.position.y + 1, portal2.transform.position.z + 1);
+                other.transform.position = new Vector3(portal2.transform.position.x + 0.4f, portal2.transform.position.y + 0.45f, portal2.transform.position.z + 0.4f);
             } else if (gameObject.CompareTag("Portal1")) {
-                other.transform.position = new Vector3(portal2.transform.position.x - 0.75f, portal2.transform.position.y + 1, portal2.transform.position.z - 1);
+                other.transform.position = new Vector3(portal2.transform.position.x - 0.4f, portal2.transform.position.y + 0.45f, portal2.transform.position.z - 0.4f);
             }
             other.transform.rotation = portal2.transform.rotation;
         }
