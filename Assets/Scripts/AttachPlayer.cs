af91cacb4d346dce5ea7ebb88ee895162b7f6150
J3rry <jerrygamerhd@gmail.com>
2022-01-30 12:30:03 +0100

    added new flawless moving platform

--- Assets/Scripts/AttachPlayer.cs
+++ Assets/Scripts/AttachPlayer.cs
@@ -5,8 +5,39 @@ using UnityEngine;
 public class AttachPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Transform PointA;
+    [SerializeField] private Transform PointB;
+    [SerializeField] private float speed=2f;
+
+    private Rigidbody rbody;
+    private Transform currentStart;
+    private Transform currentTarget;
+
+    private float distance=0f;
 
     public GameObject Player;
+
+    void Start()
+    {
+        rbody=GetComponent<Rigidbody>();
+        currentStart=PointA;
+        currentTarget=PointB;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance=distance+speed*Time.deltaTime;
+        transform.position=Vector3.Lerp(currentStart.position,currentTarget.position,distance);
+        rbody.MovePosition(Vector3.Lerp(currentStart.position,currentTarget.position,distance));
+        if(distance>1){
+            distance=0;
+            Transform oldTarget=currentTarget;
+            currentTarget=currentStart;
+            currentStart=oldTarget;
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject == Player) {
